50412bb14595caed4d1d64b131b48abd4a601d8e
Datalators <68780026+Datalators@users.noreply.github.com>
2020-07-28 01:38:37 +0600

    Update auto_click.py

--- auto_click.py
+++ auto_click.py
@@ -3,27 +3,28 @@ from time import sleep
 import winsound
 
 i = 1
-#print('Hover to active button and press Enter')
-a = (1962, 229)
-#print('Hover to send offer button and press Enter')
-b = (3104, 451)
+a = (1962, 229) #Open python from terminal to find the position
+		#Command: pyautogui.position() and press enter after moving the 
+			#mouns pointer to the position we want to click
+b = (3104, 451)	#Find the position where we want to check the color
 while True:
-	gui.click(a)
-	sleep(2)
+	gui.click(a)	#click the active button
+	sleep(2)	#wait 2 seconds to let the page load
 
 	try:
-		c = gui.pixel(a[0],b[1])
+		c = gui.pixel(b[0],b[1])	#color of the positon where the send-offer button should be
 
-		if c != (247,247,247):
-			gui.click(b)
-			winsound.Beep(1800,2000)
+		if c != (247,247,247):		# check if the color is not grey
+			gui.click(b)		#click the send-offer button
+			winsound.Beep(1800,2000)	#1800Hz frequency and 2 seconds 
 			print('Attention!')		
-			sleep(25)
+			sleep(45)		#wait extra 45 seconds if tehe is a job
 		else:
 			print(i)
-		sleep(25)
+		sleep(25)			#
 		i+=1
-	except:
+		
+	except:					#if for some reason any error arise
 		print('Trying agian')
 		winsound.Beep(2200,50)
 		sleep(15)
